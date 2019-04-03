3673b2eea1e86a53f87ff7e872b33160a445b29e
Kane <ktesta@student.unimelb.edu.au>
2019-04-03 13:04:46 +1100

    deploy server

--- exercise2.js
+++
@@ -1,8 +0,0 @@
-function getFormValue() {
-    let formName = document.getElementById("formname");
-    console.log(formName);
-    let firstName = formName.firstname.value;
-    let lastName = formName.lastname.value;
-
-    alert("Hello! " + firstName + " " + lastName);
-}
