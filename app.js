0b827d540638c558be087c3f3068435b260f64eb
Kane <ktesta@student.unimelb.edu.au>
2019-04-03 13:04:46 +1100

    deploy server

--- app.js
+++ app.js
@@ -1,46 +1,12 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
 var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-require('./models/db');
+var router = require('./routes/routes');
+var PORT = process.env.PORT || 3000;
 
-var contentRouter = require('./routes/routes-content');
-var apiRouter = require('./routes/routes-api');
-
-// View engine setup
-app.set('views', path.join(__dirname, 'src/views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', contentRouter);
-app.use('/api/', apiRouter);
-
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+app.listen(PORT, function(){ console.log(`Express listening on port ${PORT}`);
 });
 
-// Error handler
-app.use(function (err, req, res, next) {
-    // Set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // Render the error page
-    res.status(err.status || 500);
-    res.render('meta/error');
-});
+app.use('/', router);
 
-module.exports = app;
