61b72270b911f80eb5a0533d702b754a4d709dbe
Yousha <muhammadb8@student.unimelb.edu.au>
2019-04-10 12:56:41 +1000

    controller storage

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,7 @@
-var mongoose = require('mongoose')
-var Recipe = mongoose.model('recipes')
+var mongoose = require('mongoose');
+var Recipe = mongoose.model('recipes');
+var Contact = mongoose.model('contacts');
+var Storage = mongoose.model('storage');
 
 var findRecipeByIngredient = function (req, res) {
     var ingredientName = req.params.ingredient;
@@ -12,5 +14,18 @@ var findRecipeByIngredient = function (req, res) {
         }
     })
 }
+
+var findStorageInfo = function (req, res) {
+    var ingredientName = req.params.recipe;
+    Storage.find({ingredient: ingredient}, function(err, recipe){
+        if(!err){
+            res.send(storage);
+        }
+        else{
+            res.sendStatus(404);
+        }
+    })
+}
+
 
 module.exports.findRecipeByIngredient = findRecipeByIngredient;
