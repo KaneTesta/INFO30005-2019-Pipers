c002dd7a33a3ddbb05ac459163d82ee1529f0e08
James Taranto <tarantoj@student.unimelb.edu.au>
2019-04-10 23:04:44 +1000

    simplify model require

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
 
-require('./models/recipe')
+require('./models/db')
 
 var router = require('./routes/routes');
 var PORT = process.env.PORT || 3000;
