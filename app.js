9d445d4beaeb8efa01bfab429de48de3d479b2e2
Yousha <muhammadb8@student.unimelb.edu.au>
2019-04-13 00:53:59 +1000

    Contact search and post working

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 require('./models/db')
 
