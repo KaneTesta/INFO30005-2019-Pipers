c4ca2f104084f5b2a9fea34133b11e9fcd45f399
James Taranto <tarantoj@student.unimelb.edu.au>
2019-04-14 00:20:44 +1000

    Run project through prettier & standard

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,87 +1,79 @@
-var mongoose = require('mongoose');
-var Recipe = mongoose.model('recipes');
-var Contact = mongoose.model('contacts');
-var Storage = mongoose.model('storage');
+var mongoose = require('mongoose')
+var Recipe = mongoose.model('recipes')
+var Contact = mongoose.model('contacts')
+var Storage = mongoose.model('storage')
 
 var findRecipeByIngredient = function (req, res) {
-    query = req.params.ingredients.split('+')
-    Recipe.find({ingredients: {$all: query}}, function(err, recipe){
-        if(!err){
-            res.json(recipe)
-        }
-        else{
-            res.sendStatus(404);
-        }
-    })
-};
+  query = req.params.ingredients.split('+')
+  Recipe.find({ ingredients: { $all: query } }, function (err, recipe) {
+    if (!err) {
+      res.json(recipe)
+    } else {
+      res.sendStatus(404)
+    }
+  })
+}
 
 var insertRecipe = function (req, res) {
-    var recipe = new Recipe({
-        "title": req.body.title,
-        "ingredients": req.body.ingredients,
-        "method": req.body.method,
-        "author": req.body.string,
-        "serves": req.body.serves
-    });
+  var recipe = new Recipe({
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    method: req.body.method,
+    author: req.body.string,
+    serves: req.body.serves
+  })
 
-    recipe.save(function (err, newRecipe) {
-        if(!err){
-            res.send("Recipe: " + newRecipe.title + " added!");
-        }
-        else{
-            res.status(500).send({error: err});
-        }
-    });
-
+  recipe.save(function (err, newRecipe) {
+    if (!err) {
+      res.send('Recipe: ' + newRecipe.title + ' added!')
+    } else {
+      res.status(500).send({ error: err })
+    }
+  })
 }
 
 var findStorageInfo = function (req, res) {
-    var ingredientName = new RegExp('^' + req.params.ingredient, 'i');
+  var ingredientName = new RegExp('^' + req.params.ingredient, 'i')
 
-    Storage.find({ingredient: {$regex: ingredientName}}, function(err, info){
-        if(!err){
-            res.json(info);
-        }
-        else{
-            res.sendStatus(404);
-        }
-    })
-};
+  Storage.find({ ingredient: { $regex: ingredientName } }, function (err, info) {
+    if (!err) {
+      res.json(info)
+    } else {
+      res.sendStatus(404)
+    }
+  })
+}
 
 var findContact = function (req, res) {
-
-    Contact.find(req.query, function(err, info){
-        if(!err){
-            res.send(info);
-            res.sendStatus(200);
-        }
-        else{
-            res.sendStatus(404);
-        }
-    })
-};
+  Contact.find(req.query, function (err, info) {
+    if (!err) {
+      res.send(info)
+      res.sendStatus(200)
+    } else {
+      res.sendStatus(404)
+    }
+  })
+}
 
 var insertContact = function (req, res) {
-    var contact = new Contact({
-        "name": req.body.name,
-        "phone": req.body.phone,
-        "address": req.body.address
-    });
-    contact.save(function (err, newContact) {
-        if(!err){
-            res.send(name + " added!")
-        }
-        else{
-            res.status(500).send({error: err});
-        }
-    });
-};
-
+  var contact = new Contact({
+    name: req.body.name,
+    phone: req.body.phone,
+    address: req.body.address
+  })
+  contact.save(function (err, newContact) {
+    if (!err) {
+      res.send(name + ' added!')
+    } else {
+      res.status(500).send({ error: err })
+    }
+  })
+}
 
 module.exports = {
-    findRecipeByIngredient,
-    insertRecipe,
-    findStorageInfo,
-    findContact,
-    insertContact
-};
+  findRecipeByIngredient,
+  insertRecipe,
+  findStorageInfo,
+  findContact,
+  insertContact
+}
