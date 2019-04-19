1ba4bd480f4e4f6185599e4cbdd8f1b73d90d992
James Taranto <tarantoj@student.unimelb.edu.au>
2019-04-19 20:14:16 +1000

    Separate ingredients into new schema Add validation to models

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,26 +1,14 @@
 var mongoose = require('mongoose');
-var uniqueValidator = require('mongoose-unique-validator');
-
 var ingredientSchema = mongoose.Schema({
   name: {
-    type: String,
-    require: [true, 'Can\'t have an empty ingredient'],
-    trim: true,
-    lowercase: true,
-    minlength: 1,
-    unique: true,
-    get: capitalizeFirstLetter
+      type: String,
+      require: [true, 'An ingredient needs a name'],
+      trim: true,
+      lowercase: true,
+      minlength: 1
   },
-  extra: {
-    type: Map,
-    of: String
-  }
+  tip: String,
+  fridge: String,
+  pantry: String
 });
-
-function capitalizeFirstLetter(v) {
-  // Convert 'bob' -> 'Bob'
-  return v.charAt(0).toUpperCase() + v.substr(1);
-}
-
-ingredientSchema.plugin(uniqueValidator);
 mongoose.model('ingredient', ingredientSchema, 'ingredients');
