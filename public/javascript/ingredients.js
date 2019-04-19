c8356858f0ebb9be2614b54e39a6a2705f0fcf50
Joel Launder <jalaunder@gmail.com>
2019-04-19 21:32:50 +1000

    Ingredients improvements

--- public/javascript/ingredients.js
+++ public/javascript/ingredients.js
@@ -1,5 +1,31 @@
+function add_ingredient(ingredient_name) {
+    console.log("Ingredient added: " + ingredient_name);
+}
+
 $(function () {
-    $("#IngredientsSearch").autocomplete({
-        source: ["abc", "bcd", "cde"]
+    // Find ingredients
+    let ingredient_list = [
+        "Tomato",
+        "Chicken",
+        "Potato"
+    ]
+
+    // Setup autocomplete
+    let search = $("#IngredientsSearch");
+    search.autocomplete({
+        source: ingredient_list
+    });
+
+    // Setup add button
+    $("#IngredientsButtonAdd").on('click', function () {
+        console.log(search.value);
+        let ingredient = ingredient_list.find(function (element) {
+            return search.value !== undefined && element !== undefined &&
+                element.toLowerCase() === search.value.toLowerCase();
+        });
+
+        if (ingredient !== undefined) {
+            add_ingredient(ingredient);
+        }
     });
 });
