df2298930a313b10b42af2b4b2bec77680991ab7
Joel Launder <jalaunder@gmail.com>
2019-04-19 22:36:54 +1000

    Improves ingredients functionality
    Ingredients can be added, and are passed as parameters to the recipes

--- public/javascript/ingredients.js
+++ public/javascript/ingredients.js
@@ -1,31 +1,99 @@
-function add_ingredient(ingredient_name) {
-    console.log("Ingredient added: " + ingredient_name);
+/**
+ * Add an ingredient to the list of ingredients
+ */
+function addIngredient(ingredientList) {
+    let search = $("#IngredientsSearch");
+    let ingredient = ingredientList.find(function (element) {
+        return search.val() !== undefined && element !== undefined &&
+            element.toLowerCase() === search.val().toLowerCase();
+    });
+
+    if (ingredient !== undefined) {
+        if (!hasIngredient(ingredient)) {
+            // Create ingredient element
+            let ingredientsList = $("#IngredientsList");
+            let ingredientElement = $("<div class=\"ingredient-element\"></div>");
+            ingredientElement.attr("data-ingredient", ingredient);
+            ingredientElement.html(ingredient);
+            // Add to list
+            ingredientElement.appendTo(ingredientsList);
+        } else {
+            // Ingredient already added
+        }
+    } else {
+        // Ingredient not found
+    }
 }
 
-$(function () {
-    // Find ingredients
-    let ingredient_list = [
-        "Tomato",
-        "Chicken",
-        "Potato"
-    ]
+/**
+ * Get an array of the ingredients that have been added
+ * @returns {string[]}
+ */
+function getIngredients() {
+    let ingredients = [];
+    let ingredientsList = $("#IngredientsList");
+    ingredientsList.children().each(function () {
+        let element = $(this);
+        ingredients.push(element.attr("data-ingredient"));
+    });
 
+    return ingredients;
+}
+
+/**
+ * Check if an ingredient has been added to the list yet
+ * @param {string} ingredientName The name of the ingredient to check
+ * @returns {boolean}
+ */
+function hasIngredient(ingredientName) {
+    let ingredientElement = getIngredients().find(function (element) {
+        return ingredientName === element;
+    });
+
+    return ingredientElement !== undefined;
+}
+
+/**
+ * Get the ingredients that can be added
+ * @returns {string[]}
+ */
+function getAvailableIngredients() {
+    return [
+        "Tomato",
+        "Big Tomato",
+        "Chicken",
+        "Potato",
+        "Rice",
+        "Cactus"
+    ]
+}
+
+$(document).on("transition", function () {
+    // Find ingredients
+    let ingredientList = getAvailableIngredients();
     // Setup autocomplete
     let search = $("#IngredientsSearch");
     search.autocomplete({
-        source: ingredient_list
+        source: ingredientList,
+        select: function (event, ui) {
+            // Set value
+            search.val(ui.item.label);
+            // Add ingredient
+            addIngredient(ingredientList);
+            // Clear value
+            search.val("");
+            return false;
+        }
     });
 
     // Setup add button
     $("#IngredientsButtonAdd").on('click', function () {
-        console.log(search.value);
-        let ingredient = ingredient_list.find(function (element) {
-            return search.value !== undefined && element !== undefined &&
-                element.toLowerCase() === search.value.toLowerCase();
-        });
+        addIngredient(ingredientList);
+    });
 
-        if (ingredient !== undefined) {
-            add_ingredient(ingredient);
-        }
+    // Setup find recipes button
+    $("#IngredientsButtonFindRecipes").on('click', function () {
+        let ingredientsParam = getIngredients().join("+");
+        window.location.assign("./recipe?ingredients=" + ingredientsParam);
     });
 });
