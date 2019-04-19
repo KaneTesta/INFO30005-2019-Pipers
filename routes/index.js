c5e2141013eb153a622812fc90d2bb079ec475fe
Joel Launder <jalaunder@gmail.com>
2019-04-19 12:51:10 +1000

    Added progress as ejs element

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: "Home" });
+  res.render('index', { title: "Home", viewport: 0 });
 });
 
 /* GET ingredients page. */
 router.get('/ingredients', function (req, res, next) {
-  res.render('ingredients', { title: "Ingredients" });
+  res.render('ingredients', { title: "Ingredients", viewport: 1 });
 });
 
 /* GET recipe page. */
 router.get('/recipe', function (req, res, next) {
-  res.render('recipe', { title: "Recipe" });
+  res.render('recipe', { title: "Recipe", viewport: 2 });
 });
 
 module.exports = router;
