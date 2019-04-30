77f3b47ca5b2a87973ee70b720d1777229a017c3
James Taranto <tarantoj@student.unimelb.edu.au>
2019-04-30 23:15:18 +1000

    rename contact schema and update to follow mongoose convention

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Recipe = mongoose.model('recipe');
-var Contact = mongoose.model('contacts');
+var Contact = mongoose.model('contact');
 var Storage = mongoose.model('storage');
 
 // Find recipes containing one or multiple ingredients
