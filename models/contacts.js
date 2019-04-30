4c97f695bcea0f0ff643bf01bd3a415f421035e3
James Taranto <tarantoj@student.unimelb.edu.au>
2019-04-30 23:16:29 +1000

    remove rename contact schema

--- models/contacts.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var contactsSchema = mongoose.Schema({
-  name: String,
-  phone: String,
-  address: String
-});
-mongoose.model('contacts', contactsSchema, 'contacts');
