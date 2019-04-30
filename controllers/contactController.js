1da286c74d222d37ea810319f231d44f55513e43
James Taranto <tarantoj@student.unimelb.edu.au>
2019-04-30 23:30:49 +1000

    refactor contact model

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 var Recipe = mongoose.model('recipe');
-var Contact = mongoose.model('contacts');
+var Contact = mongoose.model('contact');
 var Storage = mongoose.model('storage');
