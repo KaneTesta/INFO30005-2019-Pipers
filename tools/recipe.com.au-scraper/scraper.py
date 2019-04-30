ddc5768691e70e5c519324e4f7c6904551ad5e89
James Taranto <tarantoj@student.unimelb.edu.au>
2019-05-01 00:34:00 +1000

    get all recipes from query page

--- tools/recipe.com.au-scraper/scraper.py
+++ tools/recipe.com.au-scraper/scraper.py
@@ -5,7 +5,24 @@ import requests
 import sys
 import json
 
-with requests.get(sys.argv[1]) as req:
+base = 'https://www.taste.com.au'
+
+def get_recipe(url):
+    req = requests.get(url)
     soup = BeautifulSoup(req.content)
     recipe = json.loads(soup.find('script', type='application/ld+json').text)
-    print(recipe)
+    return recipe
+
+
+def get_recipes(page):
+    url = base + f"/search-recipes/?page={page}&q=main&sort=rating"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content)
+    figures = soup.find_all('figure', class_='list-image-block')
+    links = []
+    for f in figures:
+        links.append(base + f.a['href'])
+    return links
+
+for r in get_recipes(1):
+    print(get_recipe(r))
