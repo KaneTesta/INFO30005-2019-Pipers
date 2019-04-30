9bbfee7b6a9bec291041e02b479de5a6a698abeb
James Taranto <tarantoj@student.unimelb.edu.au>
2019-04-30 23:33:34 +1000

    scrape recipe schema from taste.com.au recipe

--- tools/recipe.com.au-scraper/scraper.py
+++ tools/recipe.com.au-scraper/scraper.py
@@ -3,6 +3,9 @@
 from bs4 import BeautifulSoup
 import requests
 import sys
+import json
 
 with requests.get(sys.argv[1]) as req:
     soup = BeautifulSoup(req.content)
+    recipe = json.loads(soup.find('script', type='application/ld+json').text)
+    print(recipe)
