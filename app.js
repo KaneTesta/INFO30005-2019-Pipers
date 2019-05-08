e5f28e762a571f4642347ac2b2c0ad693b843f9c
Joel Launder <jalaunder@gmail.com>
2019-05-08 15:41:37 +1000

    Added basic oauth functionality

--- app.js
+++ app.js
@@ -9,7 +9,24 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 require('./models/db');
+
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  function (accessToken, refreshToken, profile, done) {
+    /*User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });*/
+  }
+));
 
 var contentRouter = require('./routes/routes-content');
 var apiRouter = require('./routes/routes-api');
@@ -26,6 +43,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', contentRouter);
 app.use('/api/', apiRouter);
+
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
