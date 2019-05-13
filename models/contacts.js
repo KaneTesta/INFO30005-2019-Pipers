acd990cf604e9f3ebf8106be68f44b02b8cd09c3
James Taranto <tarantoj@student.unimelb.edu.au>
2019-05-14 08:33:56 +1000

    remove deprecated schema

--- models/contacts.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const contactsSchema = mongoose.Schema({
-  name: String,
-  phone: String,
-  address: String,
-});
-mongoose.model('contacts', contactsSchema, 'contacts');
