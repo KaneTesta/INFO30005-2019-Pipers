665155cb22b28ac29cac1de486a676044e665e32
James Taranto <tarantoj@student.unimelb.edu.au>
2019-05-14 08:24:21 +1000

    Grammar fix

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   user_id: {
     type: String,
-    require: [true, 'An user needs an id'],
+    require: [true, 'A user needs an id'],
   },
   display_name: String,
   ingredients: [String],
