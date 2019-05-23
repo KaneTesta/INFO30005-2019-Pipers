873271f80e604e0b4878f8793bf739060fbac09f
Joel Launder <jalaunder@gmail.com>
2019-05-23 18:43:02 +1000

    Added recipe tips to front-end

--- public/javascript/recipe.js
+++ public/javascript/recipe.js
@@ -22,26 +22,69 @@ $(document).on("transition", function () {
         }
     });
 
+    const EJS_INGREDIENT_TIPS = `
+    <% if (data.ingredient && data.ingredient.name) { %>
+        <h5><%= data.ingredient.name %></h5>
+
+        <% if (data.ingredient.tip) { %>
+            <h6>Tip</h6>
+            <p><%= data.ingredient.tip %></p>
+        <% }; %>
+
+        <% if (data.ingredient.fridge) { %>
+            <h6>Fridge</h6>
+            <p><%= data.ingredient.fridge %></p>
+        <% }; %>
+
+        <% if (data.ingredient.pantry) { %>
+            <h6>Pantry</h6>
+            <p><%= data.ingredient.pantry %></p>
+        <% }; %>
+    <% }; %>
+    `
+
     // Handle ingredient lists
     let recipeCleanup = $("#RecipeCleanup");
     if (recipeCleanup) {
-        $.getJSON("/api/ingredients",
-            /**
-             * Get ingredients from server
-             * @param {[string]} data
-             */
-            function (data) {
-                function findIngredient(ingredient) {
-                    return data.find((el) => el.name.toLowerCase() === ingredient)
-                }
+        let totalTips = recipeCleanup.children().length;
+        let foundTips = 0;
+        // Set ingredients
+        recipeCleanup.children().each(function () {
+            let $el = $(this);
+            $el.hide();
 
-                // Set ingredients
-                recipeCleanup.children().each(function () {
-                    let $el = $(this);
-                    let ingredient = $el.attr("data-ingredient");
-                    $el.html(ingredient);
-                });
-            }
-        );
+            $.getJSON("/api/ingredient/" + $el.attr("data-ingredient"),
+                /**
+                 * Get ingredients from server
+                 * @param {[string]} data
+                 */
+                function (data) {
+                    // Update tips message
+                    ++foundTips;
+                    if (foundTips >= totalTips) {
+                        let tipsLoading = $("#RecipeCleanupLoadingTips");
+                        tipsLoading.slideUp(400, function () {
+                            tipsLoading.remove();
+                        });
+                    }
+
+                    if (data) {
+                        if (!data.tip && !data.fridge && !data.pantry) {
+                            $el.remove();
+                        } else {
+                            $el.html(ejs.render(EJS_INGREDIENT_TIPS, {
+                                data: {
+                                    ingredient: data
+                                }
+                            }));
+
+                            $el.slideDown(400);
+                        }
+                    } else {
+                        $el.remove();
+                    }
+                }
+            );
+        });
     }
 });
