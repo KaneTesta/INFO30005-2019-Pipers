d197b1ea60db2b0d70cfc19a1876d7f3bec2cb3b
James Taranto <tarantoj@student.unimelb.edu.au>
2019-05-27 14:16:37 +1000

    Add taste.com scraper

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -39,22 +39,75 @@ var findRecipeByID = (id, callback) => {
 }
 
 // Insert one recipe
-var insertRecipe = (recipe, callback) => {
-    var recipe = new Recipe({
-        title: recipe.title,
-        ingredients: recipe.ingredients,
-        method: recipe.method,
-        author: recipe.string,
-        serves: recipe.serves
-    });
-
-    recipe.save((err, newRecipe) => {
-        callback({
-            error: err,
-            result: 'Recipe: ' + newRecipe.title + ' added!'
+// Insert one recipe
+const insertRecipe = (url, callback) => {
+    request({
+      method: 'GET',
+      url,
+    }, (err, res, body) => {
+      const $ = cheerio.load(body);
+  
+      // Get recipe object
+      let obj = $("script[type='application/ld+json']");
+      const recipe = JSON.parse(obj[0].children[0].data);
+  
+      // Get ingredients object
+      obj = $("script[type='application/json']");
+      const recipeIngredients = JSON.parse(obj[0].children[0].data);
+      // Parse ingredients
+      const ingredients = recipeIngredients.ingredientSections.map((i) => {
+        const parsed = parser.parse(i.ingredient);
+  
+        // Add new ingredients
+        Ingredient.findOne({ name: i.parsedIngredient }, (iErr, iRes) => {
+          if (!iErr) {
+            if (!iRes) {
+              const newIngredient = new Ingredient({
+                name: i.parsedIngredient,
+              });
+              newIngredient.save();
+            }
+          }
         });
+  
+        return {
+          ingredient: i.parsedIngredient,
+          text: i.ingredient,
+          unit: parsed.unit,
+          quantity: parsed.quantity,
+        };
+      });
+  
+      // Get notes
+      const notes = $('.recipe-notes > div').text().trim();
+      // Get equipment
+      const equipment = $('.equipment-description').text().trim();
+  
+      // Build recipe
+      const recipeDoc = new Recipe({
+        title: recipe.name,
+        method: recipe.recipeInstructions,
+        serves: recipe.recipeYield,
+        tags: recipe.keywords,
+        description: recipe.description,
+        author: recipe.author.name,
+        source: `https:${recipe.mainEntityOfPage}` || url,
+        nutrition: recipe.nutrition,
+        prepTime: moment.duration(recipe.prepTime).asMinutes(),
+        cookTime: moment.duration(recipe.cookTime).asMinutes(),
+        totalTime: moment.duration(recipe.totalTime).asMinutes(),
+        aggregateRating: recipe.aggregateRating,
+        image: recipe.image.url,
+        notes,
+        equipment,
+        ingredients,
+      });
+      recipeDoc.save();
+      callback({
+        result: recipeDoc,
+      });
     });
-};
+  };
 
 // Find storage information for one ingredient
 var findStorageInfo = (req, res) => {
